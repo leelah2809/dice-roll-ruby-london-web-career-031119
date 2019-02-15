5dc8b6306720ef9bca64d0cb1198ebe67b76a360
Okunola Jelilat Dasola <jelilatokunola2809@gmail.com>
2019-02-15 00:23:40 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,9 +2,9 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
-def roll 
-  no = [1,2,3,4,5,6]
+def roll (no)
+   
  rand (1..6) # code goes here
  100.times no
 end
-roll
+roll([1,2,3,4,5,6])
